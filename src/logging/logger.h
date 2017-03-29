8a4b6a0b45210903dc7eb4b42e899dd749337c9e
Minh-Triet Diep <minhtrietdiep@gmail.com>
2017-03-29 13:36:42 +0200

    Logger beginning

--- src/logging/logger.h
+++ src/logging/logger.h
@@ -4,9 +4,13 @@ class Logger {
 public:
     Logger();
     ~Logger();
-    void write(std::string moduleName, 
+    void Write(std::string moduleName, 
 	       std::string functionName,
 	       Result functionResult);
-
+private:
+	std::string fileName;
+	std::string version;
+	
+	std::string getTime();
 }
 
