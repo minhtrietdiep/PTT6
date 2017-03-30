17a2ce4fb45cc9cf6726a21c111873b24bebfbc9
Minh-Triet Diep <minhtrietdiep@gmail.com>
2017-03-30 10:14:16 +0200

    Add working Logger demo

---
+++ src/logging/test.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "logger.h"
+
+int main() {
+	printf("Hello!\n");
+
+    Logger logger(/*testFile, */"0.0.0");
+	std::string logFile = logger.GetFileName();
+
+    logger.Write("test", "main", Success);
+	printf("We're done, check %s\n", logFile.c_str());
+    return 0;
+}
