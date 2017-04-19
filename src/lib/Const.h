b74930bfd0340d7b838f0b1b0bf9a7b66a94f6f5
student <i321807@student.fontys.nl>
2017-04-19 13:40:11 +0200

    fail

---
+++ src/lib/Const.h
@@ -0,0 +1,10 @@
+#pragma once 
+
+#define MAX_MESSAGE_SIZE 4096
+#define MQ_NAME_RECEIVED_MESSAGES "/ReceivedMessages"
+#define MQ_MESSAGE_PRIORITY 0
+#define MQ_MAX_MESSAGE 10
+
+#define LOG_PATH "./logjes/jemoeder.csv"
+#define LOG_PRINTLEVEL Logger::Severity::DEBUG
+#define VERSION "0.0.1"
