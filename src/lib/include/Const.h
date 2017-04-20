4ac44389abbeda878c22d7c72f2825808552d991
mt <mt@MTDebian>
2017-04-20 13:59:11 +0200

    Somewhat working MessageQueue Parser thing

--- src/lib/include/Const.h
+++ src/lib/include/Const.h
@@ -5,6 +5,6 @@
 #define MQ_MESSAGE_PRIORITY 0
 #define MQ_MAX_MESSAGE 10
 
-#define LOG_PATH "./logjes/jemoeder.csv"
+#define LOG_PATH "./logs/things.csv"
 #define LOG_PRINTLEVEL Logger::Severity::INFO
 #define VERSION "0.0.1"
