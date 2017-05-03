8df6091bd01c2e6f07007327935ab15771b6925c
Koen Brouwers <koen.brouwers@student.fontys.nl>
2017-05-03 13:14:13 +0200

    Added prints to every function call for debugging. Also removed the BL main, the main.cpp is now the new main.

--- src/BusinessLayer/Move.cpp
+++ src/BusinessLayer/Move.cpp
@@ -2,6 +2,7 @@
 
 Move::Move(int Source,int Destination,std::vector<Step> Steplist)
 {
+    std::cout << "Move:Moving from " << Source << " to " << Destination << std::endl;
 	m_Source = Source;
     m_Destination = Destination;
     m_Steplist = Steplist;
