9e6f6a4cf826469facbea5572d396c104a0ca99e
mt <mt@MTDebian>
2017-05-10 14:17:09 +0200

    Rename Error things

---
+++ src/BusinessLayer/Error.h
@@ -0,0 +1,21 @@
+#pragma once
+
+enum class ErrorCode {
+    OK,
+    ERR_UNKNOWN,
+    ERR_PARSE,
+    ERR_HW,
+    ERR_TIMEOUT
+    Size // This is just a helper to make arrays.
+};
+// Please keep this equal with ErrorCode
+const std::array<std::string, (int)ErrorCode::Size> ErrorCodeText = 
+{
+    { 
+        "OK",
+        "ERR_UNKNOWN",
+        "ERR_PARSE",
+        "ERR_HW",
+        "ERR_TIMEOUT"
+    }
+};
---
+++ src/sharedlibs/JSONError.h
@@ -0,0 +1,8 @@
+#pragma once
+
+enum class Error 
+{
+	NONE = 0,
+	VAR_NULL = -1,
+	PARSE_ERROR = -2
+};
