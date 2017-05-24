2073dafdd20078ef4e1e4054610b27dfaa8b8afe
Brouwers,Koen K.E <koen.brouwers@student.fontys.nl>
2017-05-24 11:36:31 +0200

    Update lint.sh

--- src/scripts/lint.sh
+++ src/scripts/lint.sh
@@ -1,15 +1,9 @@
-#!/bin/sh
 set -ex
-
-FOLDERS_TO_LINT=""
-CPPLINT_FLAGS='--filter=-whitespace/tab,-legal/copyright,-whitespace/line_length,-runtime/references'
+FOLDERS_TO_LINT="../BusinessLayer ../API ../HAL"
+CPPLINT_FLAGS="--counting=detailed --filter=-whitespace/tab,-legal/copyright,-whitespace/line_length,-runtime/references,-build/c++11,-build/header_guard,-build/include,-readability/casting,-readability/todo,-runtime/explicit,-runtime/threadsafe_fn,-whitespace/blank_line,-whitespace/braces,-whitespace/comma,-whitespace/comments,-whitespace/end_of_line,-whitespace/ending_newline,-whitespace/indent,-whitespace/newline,-whitespace/operators,-whitespace/parens,-whitespace/semicolon,-readability/braces,-readability/multiline_string"
 
 for FOLDER in ${FOLDERS_TO_LINT}; do
 	cpplint ${CPPLINT_FLAGS} $(
             find "${FOLDER}"
-                -type f
-                -regextype posix-extended
-                -regex '^.*\.(c|h|cpp|hpp)$'
-        ) nofile
-        echo hallo
+        ) 
 done
