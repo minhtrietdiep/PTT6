4c6fadd87080f9141f0689a0672fbc865e2cd460
student <i321807@student.fontys.nl>
2017-05-31 14:46:12 +0200

    code review HHN

--- src/BusinessLayer/Plate.cpp
+++ src/BusinessLayer/Plate.cpp
@@ -1,8 +1,14 @@
 #include "Plate.h"
+#include <Logger.h>
+#include <Const.h>
+
+#define MAX_COLLIMATORPOS_VALUE 4
+#define MIN_COLLIMATORPOS_VALUE 0
 
 
 Plate::Plate(int id, int drivePos, int collimatorPosition, std::string properties, double thickness)
 {
+	m_Logger = new Logger(VERSION,Logger::Severity::ERROR,LOG_PATH);
 	m_ID = id;
 	m_DrivePosition = drivePos;
 	m_CollimatorPosition = collimatorPosition;
@@ -29,8 +35,15 @@ int Plate::GetID()
 {
 	return m_ID;
 }
-void Plate::SetCollimatorPosition(int value)
+enum ErrorCode Plate::SetCollimatorPosition(int value)
 {
-	m_CollimatorPosition = value;
-	std::cout<<"plate set to " << value << std::endl;
+	if (value <= MAX_COLLIMATORPOS_VALUE && value >= MIN_COLLIMATORPOS_VALUE)
+	{
+		m_CollimatorPosition = value;
+		return ErrorCode::ERR_OK;
+	}
+	m_Logger->Write(Logger::Severity::ERROR, __PRETTY_FUNCTION__, "SetCollimatorPosition no int");
+	return ErrorCode::ERR_INVALID_ARG;
+
+	
 }
