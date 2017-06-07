b151ba0eb732c9bc69b1d3e4cd59ee843eee4f66
mt <mt@MTDebian>
2017-06-08 00:28:24 +0200

    Fix test makesfiles also bl is now a lib too kinda

---
+++ src/lib/include/Control.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <Error.h>
+#include <Const.h>
+#include "../../API/Interfaces/IUIControl.h"
+#include "../../API/Interfaces/IRemoteLog.h"
+#include "Preset.h"
+#include "Order.h"
+#include "Move.h"
+#include "Config.h"
+#include <Logger.h>
+#include <PlateList.h>
+
+class Control : public IUIControl, public IRemoteLog
+{
+        private:
+        std::vector<Preset> m_Presets;
+        Order m_Order;
+        Config m_Config;
+        const char* m_FileName = "Config/Presets.json";
+        Logger *m_Logger;
+
+	public:
+
+        Control(std::vector<Preset> presets);
+        ErrorCode SetupHardware();
+        //IUIControl functions
+        std::vector<Preset> GetPresets();
+        virtual ~Control();
+        virtual  ErrorCode PlateToDrive(int plateid);
+        virtual  ErrorCode PlateToCollimator(int plateid);
+        virtual  ErrorCode CancelCurrentOperation();
+        virtual  ErrorCode SetPreset(int presetid);
+        virtual  ErrorCode EmergencyStop();
+        virtual  ErrorCode ContinueSystem();
+        virtual  ErrorCode ResetSystem();
+        virtual  ErrorCode StartSystem();
+        virtual std::string UploadPresets();
+        virtual std::string UploadDriveState();
+        virtual std::string UploadCollimatorState();
+        virtual  ErrorCode LoadPresets();
+        
+
+        //IRemoteLog functions
+        virtual  ErrorCode DownloadLog(int logfilenumber);
+};
---
+++ src/lib/include/Move.h
@@ -0,0 +1,20 @@
+#ifndef MOVE_H_
+#define MOVE_H_
+
+#include <iostream>
+#include <vector>
+
+class Move {
+
+private:
+    int m_PlateID;
+    int m_Destination;
+
+public:
+    Move(int PlateID,int Destination);
+    int GetPlateID();
+    int GetDestination();
+    ~Move();
+};
+
+#endif  //  MOVE_H_
