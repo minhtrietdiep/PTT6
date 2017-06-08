e6fa77f4b0564ba7e9cee5b8ef2f41ab10f83343
mt <mt@MTDebian>
2017-06-08 10:01:01 +0200

    Fix make order

--- src/lib/include/Drive.h
+++ src/lib/include/Drive.h
@@ -4,36 +4,25 @@
 #include "Coordinates.h"
 #include "Error.h"
 #include "Const.h"
+
 #include <Logger.h>
 
 class Drive
 {
-    private:
-        int m_DriveID;
-        Coordinates m_Positions;
-        ErrorCode ToggleDrive();
-        Logger *m_Logger;
-
-        const char* DRIVE0 = "/sys/class/gpio/gpio30/value";
-        const char* DRIVE1 = "/sys/class/gpio/gpio60/value";
-        const char* DRIVE2 = "/sys/class/gpio/gpio31/value";
-        const char* DRIVE3 = "/sys/class/gpio/gpio50/value";
-        const char* DRIVE4 = "/sys/class/gpio/gpio48/value";
-
-        const char* DRIVE0D = "/sys/class/gpio/gpio30/direction";
-        const char* DRIVE1D = "/sys/class/gpio/gpio60/direction";
-        const char* DRIVE2D = "/sys/class/gpio/gpio31/direction";
-        const char* DRIVE3D = "/sys/class/gpio/gpio50/direction";
-        const char* DRIVE4D = "/sys/class/gpio/gpio48/direction";
-
-
     public:
         Drive(int driveid, Coordinates positions);
+        ~Drive();
+        ErrorCode SetupHardware();
         ErrorCode OpenDrive();
         ErrorCode CloseDrive();
         int GetDriveID();
         Coordinates GetDriveCoordinates();
-        ErrorCode SetupHardware();
+        
+    private:
+        ErrorCode ToggleDrive();
+        int m_DriveID;
+        Logger * m_Logger;
+        Coordinates m_Positions;
 };
 
 #endif  //  DRIVE_H_
