eae046eaf79c5feab070dbb02ed416fd1a2affc8
Lars Jaeqx <l.jaeqx@student.fontys.nl>
2017-06-08 10:03:54 +0200

    HAL test frame

---
+++ src/HAL/test/ArmTest.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+#include "../Arm.h"
+ 
+TEST(HAL, ArmTest1) 
+{
+    EXPECT_EQ(1, 1);
+}
---
+++ src/HAL/test/VacuumTest.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+#include "../Vacuum.h"
+ 
+TEST(HAL, VacuumTest1) 
+{
+    EXPECT_EQ(1, 1);
+}
