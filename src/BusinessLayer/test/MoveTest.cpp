c39c038fbe0e80c882338a2af96775087bdac61f
Hubert Heijnen <h.heijnen@student.fontys.nl>
2017-06-08 14:45:33 +0200

    added BL unit test

--- src/BusinessLayer/test/MoveTest.cpp
+++ src/BusinessLayer/test/MoveTest.cpp
@@ -1,9 +1,19 @@
-#include "gtest/gtest.h"
-#include "../Move.h"
- 
-TEST(Movetest, test1) 
+#include <gtest/gtest.h>
+#include <Move.h>
+
+#define ID 6
+#define DES 2 
+
+TEST(Movetest, GetPlateID) 
 {
-    EXPECT_EQ(1, 1);
+    Move move = Move(ID,DES);
+    EXPECT_EQ(move.GetPlateID(), ID);
+}
+
+TEST(Movetest, GetDestination) 
+{
+    Move move = Move(ID,DES);
+    EXPECT_EQ(move.GetDestination(), DES);
 }
 
  
